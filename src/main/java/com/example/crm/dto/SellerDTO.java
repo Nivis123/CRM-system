aa27c92ebe2b509a1d861325c28a8b8ea7dd4778
Kira <simon123172@mail.ru>
2025-08-24 20:48:54 +0700

    Add DTO for transactions and sellers

--- src/main/java/com/example/crm/dto/SellerDTO.java
+++ src/main/java/com/example/crm/dto/SellerDTO.java
@@ -1,4 +1,42 @@
 package com.example.crm.dto;
 
+import java.time.LocalDateTime;
+
 public class SellerDTO {
+    private Long id;
+    private String name;
+    private String contactInfo;
+    private LocalDateTime registrationDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
 }
---
+++ src/main/java/com/example/crm/dto/TransactionDTO.java
@@ -0,0 +1,55 @@
+package com.example.crm.dto;
+
+import com.example.crm.entity.PaymentType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class TransactionDTO {
+    private Long id;
+    private Long sellerId;
+    private BigDecimal amount;
+    private PaymentType paymentType;
+    private LocalDateTime transactionDate;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(Long sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public PaymentType getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(PaymentType paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public LocalDateTime getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDateTime transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+}
